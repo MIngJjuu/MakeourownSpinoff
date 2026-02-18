7f5b6266b67d85f7f95d03bf5d2dd35570605328
MingJjuu <frances1203@sookmyung.ac.kr>
2026-02-18 12:37:48 +0900

    로고-메인화면 연결 및 메뉴 간격 조정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 function Navbar() {
   const handleInstagramClick = () => {
-    window.open('https://www.instagram.com/your_spinoff_account', '_blank');
+    window.open('https://www.instagram.com/makeurownspinoff?igsh=MThybXJtcTgwdW9ycw==', '_blank');
   };
 
   return (
     <nav className="navbar">
+      <Link to="/" className="main-logo">
+        <img 
+          src={logo}
+          alt="Spinoff Logo"
+        />
+      </Link>
+
       <div className="menu">
         <Link to="/about" className="menu-item">About Us</Link>
         <Link to="/gathering" className="menu-item">Gathering</Link>
