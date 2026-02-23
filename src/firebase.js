48135e76ffad44664fca2d4fe9bc79f976178951
MingJjuu <frances1203@sookmyung.ac.kr>
2026-02-23 21:07:19 +0900

    호스팅을 위한 firebase 연결 및 설정(1차)

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkEuwwV8-JhR1418davjRWLgiT-VTfQOo",
+  authDomain: "makeourownspinoff.firebaseapp.com",
+  projectId: "makeourownspinoff",
+  storageBucket: "makeourownspinoff.firebasestorage.app",
+  messagingSenderId: "861661105782",
+  appId: "1:861661105782:web:a216f6a166ea038896d957"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
