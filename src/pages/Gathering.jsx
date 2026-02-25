c60e0e6953e6bd4b88ea9ab767a5c7d65836c8ad
MingJjuu <frances1203@sookmyung.ac.kr>
2026-02-25 23:38:34 +0900

    메인 페이지 패딩 재설계 및 게더링 이미지 연결

--- src/pages/Gathering.jsx
+++ src/pages/Gathering.jsx
@@ -3,28 +3,31 @@ import "./Gathering.css";
 import '../App.css';
 
 const photos = [
-  { id: 1, src: "src/assets/melancholia.jpg", caption: "멜랑콜리아" },
-  { id: 2, src: "src/assets/kimsvideo.jpg", caption: "킴스비디오" },
-  { id: 3, src: "src/assets/kirishima.jpg", caption: "키리시마가 동아리활동 그만둔대" },
-  { id: 4, src: "src/assets/her.jpg", caption: "그녀" },
-  { id: 5, src: "src/assets/monster.jpg", caption: "괴물" },
-  { id: 6, src: "src/assets/In the Mood for Love.jpg", caption: "화양연화" },
-  { id: 7, src: "src/assets/minari.jpg", caption: "미나리" },
-  { id: 8, src: "src/assets/The Phantom of Baker Street.png", caption: "명탐정 코난:베이커 가의 망령" },
-  { id: 9, src: "src/assets/Anatomy of a fall.png", caption: "추락의 해부" },
-  { id: 10, src: "src/assets/singstreet.png", caption: "싱 스트리트" },
-  { id: 11, src: "src/assets/Comrades.png", caption: "첨밀밀" },
-  { id: 12, src: "src/assets/getout.png", caption: "겟 아웃" },
-  { id: 13, src: "src/assets/Decision To Leave.png", caption: "헤어질 결심" },
-  { id: 14, src: "src/assets/BeauisAfraid.webp", caption: "보 이즈 어프레이드" },
-  { id: 15, src: "src/assets/TheFall.jpg", caption: "더 폴" }
+  { id: 1, src: "src/assets/melancholia.jpg", caption: "멜랑콜리아", link: "https://www.instagram.com/p/DU7zR1SE75z/" },
+  { id: 2, src: "src/assets/kimsvideo.jpg", caption: "킴스비디오", link: "https://www.instagram.com/p/DU7y_4ok3V9/" },
+  { id: 3, src: "src/assets/kirishima.jpg", caption: "키리시마가 동아리활동 그만둔대", link: "https://www.instagram.com/p/DT9umkSEzHY/" },
+  { id: 4, src: "src/assets/her.jpg", caption: "그녀", link: "https://www.instagram.com/p/DT9uMPmk4CN/" },
+  { id: 5, src: "src/assets/monster.jpg", caption: "괴물", link: "https://www.instagram.com/p/DT9tjGWk9p0/" },
+  { id: 6, src: "src/assets/In the Mood for Love.jpg", caption: "화양연화", link: "https://www.instagram.com/p/DT9snIZkwEI/" },
+  { id: 7, src: "src/assets/minari.jpg", caption: "미나리", link: "https://www.instagram.com/p/DT9r39XEyfc/" },
+  { id: 8, src: "src/assets/The Phantom of Baker Street.png", caption: "명탐정 코난:베이커 가의 망령", link: "https://www.instagram.com/p/DT9rmk9E-FM/" },
+  { id: 9, src: "src/assets/Anatomy of a fall.png", caption: "추락의 해부", link: "https://www.instagram.com/p/DT9rfpBk5XJ/" },
+  { id: 10, src: "src/assets/singstreet.png", caption: "싱 스트리트", link: "https://www.instagram.com/p/DT9rE3QExva/" },
+  { id: 11, src: "src/assets/Comrades.png", caption: "첨밀밀", link: "https://www.instagram.com/p/DT9qfVwE-9L/" },
+  { id: 12, src: "src/assets/getout.png", caption: "겟 아웃", link: "https://www.instagram.com/p/DT9qfVwE-9L/" },
+  { id: 13, src: "src/assets/Decision To Leave.png", caption: "헤어질 결심", link: "https://www.instagram.com/p/DT9qN5qE_LQ/" },
+  { id: 14, src: "src/assets/BeauisAfraid.webp", caption: "보 이즈 어프레이드", link: "https://www.instagram.com/p/DT9pp_wE69h/" },
+  { id: 15, src: "src/assets/TheFall.jpg", caption: "더 폴", link: "https://www.instagram.com/p/DT9o44KEy6r/" }
 ];
 
 export default function Gathering() {
   const [selected, setSelected] = useState(null);
 
-  const openModal = (photo) => setSelected(photo);
-  const closeModal = () => setSelected(null);
+  const handleClick = (photo) => {
+    if (photo.link) {
+      window.open(photo.link, '_blank'); // 새 탭에서 열기
+    }
+  };
 
   return (
     <div className="gathering-page">
@@ -51,16 +54,6 @@ export default function Gathering() {
           </div>
         ))}
       </div>
-
-      {selected && (
-        <div className="modal-backdrop" onClick={closeModal}>
-          <div className="modal-box" onClick={(e) => e.stopPropagation()}>
-            <button className="modal-close" onClick={closeModal}>✕</button>
-            <img src={selected.src} alt={selected.alt} />
-            <p className="modal-caption">{selected.caption}</p>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
